7e7ea56e520489a20effb076d5d69fccd99e7d2d
Vadim Brodsky <vadimb@crankworks.ca>
2015-05-15 10:32:26 -0400

    Add guide functionality

--- ttt.rb
+++ ttt.rb
@@ -13,9 +13,9 @@ HORIZONTAL_LINES = "-----+-----+-----"
 PLAYER_PIECE   = 'X'
 COMPUTER_PIECE = 'O'
 
-def initialize_board(guide=false)
+def initialize_board(numbers=false)
   board = {}
-  if guide
+  if numbers
     BOARD_POSITIONS.each { |i| board[i] = i }
   else
     BOARD_POSITIONS.each { |i| board[i] = false }
@@ -38,12 +38,29 @@ def draw_board(b)
   puts ""
 end
 
+def draw_guide_board(b)
+  guide_board = b.dup
+
+  system("clear")
+  puts "Guide of Positions \n\n\n"
+
+  guide_board.each do |key, value|
+    if value == false
+      guide_board[key] = key
+    end
+  end
+
+  draw_board(guide_board)
+end
+
+
 def available_positions(b)
   board = b.dup
   board.delete_if do |key, value|
     b[key]
   end
 end
+
 
 def computer_choose_position(b)
   positions = available_positions(b)
@@ -63,11 +80,13 @@ def horizontal_victory(b)
   win
 end
 
+
 def vertical_victory(b)
   win = false
   (1..3).each {|i| win = b[i] if ( b[i] == b[i + 3] ) && ( b[i + 3] == b[i + 6]) }
   win
 end
+
 
 def diagonal_victory(b)
   if ( (b[1] == b[1 + 4]) && (b[1 + 4] == b[1 + 8]) ) ||
@@ -91,6 +110,7 @@ def end_game(board)
   end
 end
 
+
 system("clear")
 puts "Welcome to Tic-Tac-Toe! \n\n\n"
 
@@ -98,13 +118,22 @@ puts "Welcome to Tic-Tac-Toe! \n\n\n"
 draw_board(initialize_board(true))
 game_board = initialize_board
 
+
+# Main Game Loop
 begin
 
   # Get user position
   begin
-    print "Choose a position (1 to 9) to place 'X': "
-    player_position = gets.chomp.to_i
-  end until BOARD_POSITIONS.include?(player_position) && position_not_taken?(game_board, player_position)
+    print "Choose a position (1 to 9) to place 'X' or G for guide: "
+    player_position = gets.chomp
+
+    if player_position.downcase == 'g'
+      draw_guide_board(game_board)
+    end
+    player_position = player_position.to_i
+
+  end until BOARD_POSITIONS.include?(player_position) &&
+                            position_not_taken?(game_board, player_position)
 
   game_board[player_position] = PLAYER_PIECE
 
@@ -120,7 +149,7 @@ begin
   # Draw new board
   draw_board(game_board)
 
-  # binding.pry
+
 end until end_game(game_board)
 
 puts "Game Over!"
