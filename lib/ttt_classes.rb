b7847c573f06cf29077c2cdc4f0061772a90e1ef
Vadim Brodsky <vadimb@crankworks.ca>
2015-06-02 08:36:53 -0400

    Add computer move, refactor blank cell checking

--- lib/ttt_classes.rb
+++ lib/ttt_classes.rb
@@ -35,6 +35,10 @@ class Cell
     column == 3
   end
 
+  def is_blank?
+    mark == ' '
+  end
+
   private
 
   def set_row(i)
@@ -68,7 +72,7 @@ class Board
   def draw_guide_board
     @board.each do |cell|
       guide_cell = cell.dup
-      guide_cell.mark = cell.mark == ' ' ? cell.index : cell.mark
+      guide_cell.mark = cell.is_blank? ? cell.index : cell.mark
       print_row(guide_cell)
     end
   end
@@ -80,9 +84,15 @@ class Board
   end
 
   def mark(position, mark = 'X')
-    if @board[position - 1].mark == ' '
-      @board[position - 1].mark = mark
+    @board[position - 1].mark = mark if @board[position - 1].is_blank?
+  end
+
+  def blank_spaces
+    spaces = []
+    board.each do |cell|
+      spaces << cell.index if cell.is_blank?
     end
+    spaces
   end
 end
 
@@ -104,12 +114,23 @@ class Game
 
   def play
     loop do
-      ask_player_position == 'g' ? draw_guide_board : redraw_borad
+      player_position = ask_input
+      if player_position == 'g'
+        draw_guide_board
+        next
+      end
+
+      if @ttt.blank_spaces.include?(player_position.to_i)
+        player_make_mark(player_position.to_i)
+        computer_make_mark
+      end
+
+      redraw_borad
     end
   end
 
   def redraw_borad
-    system('clear')
+    # system('clear')
     puts "\n\n"
     @ttt.draw_board
   end
@@ -121,15 +142,6 @@ class Game
     puts ''
   end
 
-  def ask_player_position
-    player_position = ask_input
-    if player_position == 'g'
-      'g'
-    else
-      @ttt.mark(player_position.to_i, 'X')
-    end
-  end
-
   def ask_input
     input = ''
     loop do
@@ -138,6 +150,15 @@ class Game
       break if %w(1 2 3 4 5 6 7 8 9 g).include? input
     end
     input
+  end
+
+  def player_make_mark(position)
+    @ttt.mark(position, 'X')
+  end
+
+  def computer_make_mark
+    computer_position =  @ttt.blank_spaces.sample
+    @ttt.mark(computer_position, 'O')
   end
 end
 
